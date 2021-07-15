2019c29e7ed6388ae0d87882c0a117633b55acd9
iram-afsha <85054305+iram-afsha@users.noreply.github.com>
2021-07-15 20:41:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+var a = prompt('enter the variable')
+
+function setup() {
+  createCanvas(400, 400);  
+  console.log("value of a is :" +a)
+  var b2 = createButton("press here to swap")
+  
+
+}
+
+function draw() {
+  background(220);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var a = prompt("enter the first varibale :" +a);
+
+
+function setup() {
+  createCanvas(400, 400);
+  var b2 = createButton("click here to swap");
+
+}
+*/
